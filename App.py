78b55873f2f9b2b8d956be7a4c8ddbae37862cec
Murillo Rocha <murillorobaina26@gmail.com>
2026-05-25 21:09:16 -0300

    Update App.py

--- App.py
+++ App.py
@@ -1,12 +1,25 @@
-from flask import Flask
+from flask import Flask, request, redirect, render_template
+from Tarefas import tarefas, adicionar_tarefa, completar_tarefa, carregar_dado
 
 app = Flask(__name__)
-
-
+print(app.template_folder) 
 @app.route('/')
 def index():
-    return ('Hello World!')
+    tarefas_classificadas = sorted(tarefas, key=lambda t: t['feito'])
+    return render_template('index.html', tarefas=tarefas_classificadas)
 
+@app.route('/adicionar', methods=['POST'])
+def adicionar():
+    texto_tarefa = request.form.get('texto_tarefa')
+    if texto_tarefa:
+        adicionar_tarefa(texto_tarefa)
+    return redirect('/')
+
+@app.route('/completar/<int:id>')
+def completo(id):
+    completar_tarefa(id)
+    return redirect('/')
 
 if __name__ == '__main__':
+    carregar_dado()
     app.run(debug=True)
