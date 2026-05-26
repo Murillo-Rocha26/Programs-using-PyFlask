de40de3a593c512416fd9199a9b016eb2dfa7dd2
Murillo Rocha <murillorobaina26@gmail.com>
2026-05-25 21:10:10 -0300

    Update json.py

--- json.py
+++ json.py
@@ -1,21 +1,34 @@
 import json
 
+tarefas = []
+proximo_id = 1
 
 def salvar_dado():
     with open('tarefas.json', 'w') as f:
-        json.dump({'proximo_id': proximo_id, 'tarefas': 'tarefas'}, f)
-
+        json.dump({'proximo_id': proximo_id, 'tarefas': tarefas}, f)
 
 def carregar_dado():
     global proximo_id, tarefas
+    try:
+        with open('tarefas.json', 'r') as f:
+            data = json.load(f)
+            tarefas = data['tarefas']
+            proximo_id = data['proximo_id']
+    except FileNotFoundError:
+        pass
 
+def adicionar_tarefa(texto):
+    global proximo_id
+    tarefas.append({
+        'id': proximo_id,
+        'texto': texto,
+        'feito': False
+    })
+    proximo_id += 1
+    salvar_dado()
 
-try:
-    with open('tarefas.json', 'r') as f:
-        data = json.load(f)
-    tarefas = data['tarefas']
-    proximo_id = dado['proximo_id']
-
-
-except FileNotFoundError:
-    pass
+def completar_tarefa(id):
+    for tarefa in tarefas:
+        if tarefa['id'] == id:
+            tarefa['feito'] = True
+            salvar_dado()
